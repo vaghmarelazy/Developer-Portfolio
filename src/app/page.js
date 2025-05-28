fa5e420881b0c21059eea13fb32b452dd8fb8946
Rupesh Vaghmare <rvaghmarealone@gmail.com>
2025-05-29 00:23:52 +0530

    Final touch

--- src/app/page.js
+++ src/app/page.js
@@ -33,8 +33,8 @@ const projects = [
     icon: "/projects/MystryMessages.png",
     descrption: "A full-stack anonymous messaging platform where users can send and receive mystery messages securely. Built with Next.js, Resend, MongoDB, and Gemini AI, this app offers a seamless and interactive user experience.",
     isLinkAvailable: true,
-    liveLink:"https://mystry-messages-delta.vercel.app/",
-    github : "https://github.com/vaghmarelazy/Mystry-messages"
+    liveLink: "https://mystry-messages-delta.vercel.app/",
+    github: "https://github.com/vaghmarelazy/Mystry-messages"
 
   },
   {
@@ -42,23 +42,39 @@ const projects = [
     icon: "/projects/FocusFlow.png",
     descrption: "FocusFlow is a clean, full-screen Android timer app designed to help you stay productive using the Pomodoro technique (work + break cycles). Built using Kotlin, this app is lightweight, intuitive, and completely open-source.",
     isLinkAvailable: false,
-    github : "https://github.com/vaghmarelazy/FocusFlow"
+    github: "https://github.com/vaghmarelazy/FocusFlow"
   },
   {
     name: "APOD",
     icon: "/projects/APOD.png",
     descrption: "A React application that fetches and displays data from NASA's Astronomy Picture of the Day (APOD) API, including the title, description, and image of the day.",
-    isLinkAvailable : true,
-    liveLink:"https://apod-nasa-react-psi.vercel.app/",
-    github : "https://github.com/vaghmarelazy/APOD-NASA"
+    isLinkAvailable: true,
+    liveLink: "https://apod-nasa-react-psi.vercel.app/",
+    github: "https://github.com/vaghmarelazy/APOD-NASA"
   },
   {
     name: "PassOP",
     icon: "/projects/PassOP.png",
     descrption: "A react application with Clean UI to store password both in localstorage and Cloud (MongoDB)",
+    isLinkAvailable: true,
+    liveLink: "https://lazymanager.netlify.app/",
+    github: "https://github.com/vaghmarelazy/Password-Manger"
+  },
+  {
+    name: "SGSU (Re-Designed)",
+    icon : "/projects/SGSU.png",
+    descrption:"A Re-designed work of my college website added some animations with GSAP and smooth scrolling for a great experience and to look cooler than the typical wordpress site",
+    isLinkAvailable: true,
+    liveLink:"https://sgsu-project.vercel.app/",
+    github: "https://github.com/vaghmarelazy/SGSU-Project"
+  },
+  {
+    name: "Workout generator",
+    icon : "/projects/Workout.png",
+    descrption:"A workout Generator application build on React.js + TailwindCSS had multiple choice to select a workout session ",
     isLinkAvailable : true,
-    liveLink:"https://lazymanager.netlify.app/",
-    github : "https://github.com/vaghmarelazy/Password-Manger"
+    liveLink : "https://workout-react-app.vercel.app/",
+    github : "https://github.com/vaghmarelazy/workout-react-app"
   }
 ]
 
@@ -97,10 +113,10 @@ export default function Portfolio() {
       <ThemeToggle onThemeChange={handleThemeChange} />
 
       <section className="min-h-screen flex flex-col justify-center text-center space-y-3 sm:space-y-4 duration-500">
+        {/* <p className="text-base sm:text-lg lg:text-2xl opacity-75">Software developer</p> */}
         <h1 className={`text-4xl sm:text-5xl lg:text-6xl font-bold ${sora.className}`}>Rupesh Vaghmare</h1>
-        <p className="text-base sm:text-lg opacity-75">Full-Stack Developer (MERN Stack)</p>
-        <p className="max-w-3xl mx-auto px-4 text-sm sm:text-base opacity-70 font-light">Full-Stack Developer specializing in the MERN stack.
-          I build dynamic web applications with clean UI, scalable backends, and real-time features. I&apos;ve developed projects like MystryMessages, Pass OP (password manager), and an Android appilication FocusFlow. Currently seeking new opportunities to build impactful tech solutions.</p>
+        <p className="max-w-3xl mx-auto px-4 text-xs sm:text-xs lg:text-sm opacity-70 font-light">I&apos;m a Software Developer specializing in the MERN stack.
+          I build dynamic web applications with clean UI, scalable backends, and real-time features. I&apos;ve developed projects like MystryMessages, Pass OP, and an Android appilication FocusFlow. Currently seeking new opportunities to build impactful tech solutions.</p>
         <div className="flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4">
           <Button asChild>
             <a href="https://github.com/vaghmarelazy" target="_blank">
@@ -117,28 +133,58 @@ export default function Portfolio() {
 
       {/* Projects Section */}
       <section className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20">
-        <h2 className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-extrabold mb-6 text-center">Projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 md:gap-6 w-full">
-          {/* Projects */}
-          {projects.map((project) => (
-            <Card key={project.name}>
-              <CardContent className="p-4">
-                <Image src={project.icon} alt="Live preview image" width={500} height={500} className=" rounded-md mb-3 w-full h-60" />
-                <h3 className="text-lg md:text-xl font-bold">{project.name}</h3>
-                <p className="text-sm text-muted-foreground mt-1 mb-3">{project.descrption}</p>
-                <div className="mt-4 flex flex-col sm:flex-row gap-2">
-                  <Button asChild variant="outline"  className={`${project.isLinkAvailable?'cursor-pointer ':'cursor-not-allowed'}`}><a href={project.liveLink} target="_blank">Live</a></Button>
-                  <Button asChild variant="outline"><a href={project.github} target="_blank">Code</a></Button>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
-      </section>
+  <div className="flex items-center space-x-2 mb-4">
+    <div className="w-7 h-[1px] bg-black dark:bg-white" />
+    <p className="text-xs sm:text-base lg:text-sm opacity-90 uppercase">What I have done</p>
+  </div>
+
+  <h2 className="text-4xl sm:text-6xl md:text-5xl lg:text-6xl font-extrabold mb-10 text-center">
+    Projects
+  </h2>
+
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-7xl">
+    {projects.map((project) => (
+      <Card key={project.name}>
+        <CardContent className="p-4 flex flex-col h-full">
+          <div className="mb-4">
+            <Image
+              src={project.icon}
+              alt="Live preview image"
+              width={500}
+              height={300}
+              className="rounded-md w-full object-cover"
+            />
+          </div>
+          <h3 className="text-lg md:text-xl font-bold">{project.name}</h3>
+          <p className="text-sm text-muted-foreground mt-2 flex-grow">
+            {project.descrption}
+          </p>
+          <div className="mt-4 flex flex-col sm:flex-row gap-2">
+            <Button
+              asChild
+              variant="outline"
+              className={`${project.isLinkAvailable ? 'cursor-pointer' : 'cursor-not-allowed'}`}
+            >
+              <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                Live
+              </a>
+            </Button>
+            <Button asChild variant="outline">
+              <a href={project.github} target="_blank" rel="noopener noreferrer">
+                Code
+              </a>
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    ))}
+  </div>
+</section>
+
 
       {/* Skills Section */}
       <section className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-6 lg:px-8 text-center">
-        <h1 className="text-xs sm:text-sm md:text-md font-bold uppercase mb-3 sm:mb-5 opacity-70">I CONSTANTLY TRY TO IMPROVE</h1>
+        <h1 className="text-xs sm:text-sm md:text-md font-bold uppercase mb-3 sm:mb-5 opacity-80 tracking-wide">I CONSTANTLY TRY TO IMPROVE</h1>
         <h2 className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl mb-8 sm:mb-12 md:mb-16 lg:mb-20">My tech stack </h2>
         <div className="flex flex-wrap items-center justify-center gap-x-4 gap-y-3 w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-3xl">
           {tech.map((tech) => (
@@ -148,17 +194,19 @@ export default function Portfolio() {
             </div>
           ))}
         </div>
+        <p className="mt-4 opacity-90">and it&apos;s just strating</p>
 
       </section>
 
 
       {/* About Section */}
       <section className="min-h-screen flex flex-col justify-center max-w-xl md:max-w-2xl mx-auto text-center px-4 py-12">
-        <h2 className="text-2xl sm:text-3xl lg:text-4xl font-semibold mb-4">About Me</h2>
+        <h2 className="text-2xl sm:text-3xl lg:text-5xl font-semibold mb-4">About Me</h2>
         <p className="text-sm sm:text-base opacity-80">
-          I&apos;m a MERN stack developer based in Bhopal pursuing B.Tech in Computer & Engineering Science at SGSU University (2021–2025).
-          Passionate about building real-time web applications and exploring AI-powered solutions. I&apos;m little bit in Everything. Looking for opportunities ;)
+          I&apos;m a MERN stack developer based in Bhopal pursued B.Tech in Computer & Engineering Science at SGSU University (2021–2025).
+          Passionate about building real-time web applications and exploring AI-powered solutions.I&apos;m Jack of all trades I&apos;m little bit in Everything. I do Gaming, Streming, Content Creation, Photography and much more.
         </p>
+        <p className="text-sm sm:text-base opacity-80"> Currently Looking for opportunities in tech</p>
       </section>
 
       {/* Contact Section */}
@@ -166,6 +214,7 @@ export default function Portfolio() {
         <h2 className="text-xl sm:text-2xl font-semibold">Get in Touch</h2>
         <p className="text-sm sm:text-base">Email: <a className="text-yellow-500 hover:text-yellow-600 duration-300" href="mailto:rupeshvaghmare1@email.com">rupeshvaghmare1@gmail.com</a></p>
       </section>
+      <footer className="w-full text-center">Made with {theme=='dark'?'🤍':'❤️'} by Me</footer>
     </div>
   );
 }
