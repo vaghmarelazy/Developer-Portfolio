3da08e33c8de43730a8c485b40fe5743d2eb408a
Rupesh Vaghmare <rvaghmarealone@gmail.com>
2025-05-29 10:34:48 +0530

    Refactor layout and page components for improved readability and consistency

--- src/app/page.js
+++ src/app/page.js
@@ -62,19 +62,19 @@ const projects = [
   },
   {
     name: "SGSU (Re-Designed)",
-    icon : "/projects/SGSU.png",
-    descrption:"A Re-designed work of my college website added some animations with GSAP and smooth scrolling for a great experience and to look cooler than the typical wordpress site",
+    icon: "/projects/SGSU.png",
+    descrption: "A Re-designed work of my college website added some animations with GSAP and smooth scrolling for a great experience and to look cooler than the typical wordpress site",
     isLinkAvailable: true,
-    liveLink:"https://sgsu-project.vercel.app/",
+    liveLink: "https://sgsu-project.vercel.app/",
     github: "https://github.com/vaghmarelazy/SGSU-Project"
   },
   {
     name: "Workout generator",
-    icon : "/projects/Workout.png",
-    descrption:"A workout Generator application build on React.js + TailwindCSS had multiple choice to select a workout session ",
-    isLinkAvailable : true,
-    liveLink : "https://workout-react-app.vercel.app/",
-    github : "https://github.com/vaghmarelazy/workout-react-app"
+    icon: "/projects/Workout.png",
+    descrption: "A workout Generator application build on React.js + TailwindCSS had multiple choice to select a workout session ",
+    isLinkAvailable: true,
+    liveLink: "https://workout-react-app.vercel.app/",
+    github: "https://github.com/vaghmarelazy/workout-react-app"
   }
 ]
 
@@ -99,8 +99,8 @@ export default function Portfolio() {
 
   const handleThemeChange = (newThemeFromChild) => {
     // console.log("Parent component received theme:", newThemeFromChild);
-    setTheme(newThemeFromChild); // Update the parent's theme state
-    localStorage.setItem('theme', newThemeFromChild); // Persist to localStorage
+    setTheme(newThemeFromChild); 
+    localStorage.setItem('theme', newThemeFromChild); 
     // Update the class on the root HTML element
     document.documentElement.classList.remove('light', 'dark');
     document.documentElement.classList.add(newThemeFromChild);
@@ -115,7 +115,7 @@ export default function Portfolio() {
       <section className="min-h-screen flex flex-col justify-center text-center space-y-3 sm:space-y-4 duration-500">
         {/* <p className="text-base sm:text-lg lg:text-2xl opacity-75">Software developer</p> */}
         <h1 className={`text-4xl sm:text-5xl lg:text-6xl font-bold ${sora.className}`}>Rupesh Vaghmare</h1>
-        <p className="max-w-3xl mx-auto px-4 text-xs sm:text-xs lg:text-sm opacity-70 font-light">I&apos;m a Software Developer specializing in the MERN stack.
+        <p className="max-w-3xl mx-auto px-4 font-extralight text-xs sm:text-xs lg:text-sm opacity-70 ">I&apos;m a Software Developer specialized in the MERN stack.
           I build dynamic web applications with clean UI, scalable backends, and real-time features. I&apos;ve developed projects like MystryMessages, Pass OP, and an Android appilication FocusFlow. Currently seeking new opportunities to build impactful tech solutions.</p>
         <div className="flex flex-col sm:flex-row justify-center items-center gap-2 sm:gap-4">
           <Button asChild>
@@ -133,53 +133,53 @@ export default function Portfolio() {
 
       {/* Projects Section */}
       <section className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20">
-  <div className="flex items-center space-x-2 mb-4">
-    <div className="w-7 h-[1px] bg-black dark:bg-white" />
-    <p className="text-xs sm:text-base lg:text-sm opacity-90 uppercase">What I have done</p>
-  </div>
+        <div className="flex items-center space-x-2 mb-4">
+          <div className="w-7 h-[1px] bg-black dark:bg-white" />
+          <p className="text-xs sm:text-base lg:text-sm opacity-90 uppercase">What I have done</p>
+        </div>
 
-  <h2 className="text-4xl sm:text-6xl md:text-5xl lg:text-6xl font-extrabold mb-10 text-center">
-    Projects
-  </h2>
+        <h2 className="text-4xl sm:text-6xl md:text-5xl lg:text-6xl font-extrabold mb-10 text-center">
+          Projects
+        </h2>
 
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-7xl">
-    {projects.map((project) => (
-      <Card key={project.name}>
-        <CardContent className="p-4 flex flex-col h-full">
-          <div className="mb-4">
-            <Image
-              src={project.icon}
-              alt="Live preview image"
-              width={500}
-              height={300}
-              className="rounded-md w-full object-cover"
-            />
-          </div>
-          <h3 className="text-lg md:text-xl font-bold">{project.name}</h3>
-          <p className="text-sm text-muted-foreground mt-2 flex-grow">
-            {project.descrption}
-          </p>
-          <div className="mt-4 flex flex-col sm:flex-row gap-2">
-            <Button
-              asChild
-              variant="outline"
-              className={`${project.isLinkAvailable ? 'cursor-pointer' : 'cursor-not-allowed'}`}
-            >
-              <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
-                Live
-              </a>
-            </Button>
-            <Button asChild variant="outline">
-              <a href={project.github} target="_blank" rel="noopener noreferrer">
-                Code
-              </a>
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-    ))}
-  </div>
-</section>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-7xl">
+          {projects.map((project) => (
+            <Card key={project.name}>
+              <CardContent className="p-4 flex flex-col h-full">
+                <div className="mb-4">
+                  <Image
+                    src={project.icon}
+                    alt="Live preview image"
+                    width={500}
+                    height={300}
+                    className="rounded-md w-full object-cover"
+                  />
+                </div>
+                <h3 className="text-lg md:text-xl font-bold">{project.name}</h3>
+                <p className="text-sm text-muted-foreground mt-2 flex-grow">
+                  {project.descrption}
+                </p>
+                <div className="mt-4 flex flex-col sm:flex-row gap-2">
+                  <Button
+                    asChild
+                    variant="outline"
+                    className={`${project.isLinkAvailable ? 'cursor-pointer' : 'cursor-not-allowed'}`}
+                  >
+                    <a href={project.liveLink} target="_blank" rel="noopener noreferrer">
+                      Live
+                    </a>
+                  </Button>
+                  <Button asChild variant="outline">
+                    <a href={project.github} target="_blank" rel="noopener noreferrer">
+                      Code
+                    </a>
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </section>
 
 
       {/* Skills Section */}
@@ -204,7 +204,7 @@ export default function Portfolio() {
         <h2 className="text-2xl sm:text-3xl lg:text-5xl font-semibold mb-4">About Me</h2>
         <p className="text-sm sm:text-base opacity-80">
           I&apos;m a MERN stack developer based in Bhopal pursued B.Tech in Computer & Engineering Science at SGSU University (2021–2025).
-          Passionate about building real-time web applications and exploring AI-powered solutions.I&apos;m Jack of all trades I&apos;m little bit in Everything. I do Gaming, Streming, Content Creation, Photography and much more.
+          Passionate about building real-time web applications and exploring AI-powered solutions.I&apos;m Jack of all trades I&apos;m little bit in Everything. I do Gaming, Streming, Content Creation, Photography and much more things.
         </p>
         <p className="text-sm sm:text-base opacity-80"> Currently Looking for opportunities in tech</p>
       </section>
@@ -214,7 +214,7 @@ export default function Portfolio() {
         <h2 className="text-xl sm:text-2xl font-semibold">Get in Touch</h2>
         <p className="text-sm sm:text-base">Email: <a className="text-yellow-500 hover:text-yellow-600 duration-300" href="mailto:rupeshvaghmare1@email.com">rupeshvaghmare1@gmail.com</a></p>
       </section>
-      <footer className="w-full text-center">Made with {theme=='dark'?'🤍':'❤️'} by Me</footer>
+      <footer className="w-full text-center">Made with {theme == 'dark' ? '🤍' : '❤️'} by Me</footer>
     </div>
   );
 }
